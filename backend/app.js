5acd0f32fb36b76c7ffcf3880429f3c2593c42a2
Tobi Oladimeji <jigah4july@gmail.com>
2022-06-22 18:52:23 +0100

    modify app.js

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,17 @@ app.get('/', (req, res) => {
 
 app.use('/api/todos', todoRouter)
 
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+  
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+    );
+  } else {
+      app.get('*', (req, res) => res.send('Please set to production environment'))
+  }
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
