344da8ddeb68469809f32296a7d794b12a56b207
Tobi Oladimeji <jigah4july@gmail.com>
2022-06-22 17:54:53 +0100

    App update

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import todoRouter from './routes/todoRouter.js'
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todoReact', {
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:3000/todoReact', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
